5142edba11ef93fd35303497fd71d901771eaaa8
Kim Chase <me@hi-im.kim>
2023-11-11 13:37:30 -0500

    Moved to proper template structure for main pages, added upcoming matches table, still need API plumbing for this.

--- src/app.py
+++ src/app.py
@@ -6,4 +6,8 @@ app.register_blueprint(user_screens, url_prefix='/screens')
 
 @app.route('/')
 def index():
-    return render_template("base.html", user_screens=user_screens)
+    return render_template("homepage.html", user_screens=user_screens)
+
+@app.route('/upcoming')
+def routeForUpcomingMatches():
+    return render_template("upcoming.html", upcoming_matches=[])
