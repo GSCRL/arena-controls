b60503942a3abec03b8b675c05a12429bf099ecc
Kim Chase <me@hi-im.kim>
2023-11-11 14:34:22 -0500

    Added super basic timer guts to the statuspage for match controls.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 from screens.user_screens import user_screens
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 app.register_blueprint(user_screens, url_prefix='/screens')
+
+app.config['SECRET_KEY'] = 'secret secret key (required)!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -11,3 +15,13 @@ def index():
 @app.route('/upcoming')
 def routeForUpcomingMatches():
     return render_template("upcoming.html", upcoming_matches=[])
+
+@socketio.on('timer_event')
+def handle_message(timer_data):
+    print(f'match timer is set to {timer_data}')
+    emit('timer_event', timer_data, broadcast=True)
+
+   
+if __name__ == '__main__':
+    socketio.run(app)
+
