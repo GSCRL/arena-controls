b5335b9911f02b90bb7644cfc2625bb2de4fdaa8
Kim <MC42@users.noreply.github.com>
2023-12-02 11:55:04 -0500

    Added exception to remove byes, yay!

--- src/truefinals_api/wrapper.py
+++ src/truefinals_api/wrapper.py
@@ -38,26 +38,32 @@ class TrueFinals:
         matches_nonzero = self.getGamesWithNonZeroCompetitors(tournamentID)
         competitors = self.getAllPlayersOfTournament(tournamentID)
 
-        pprint(matches_nonzero)
-        pprint(competitors)
-
         def playerIDToName(competitors, playerID: str):
             for c in competitors:
                 if c["id"] == playerID:
                     return c
             if playerID.startswith("bye"):
-                return {"name":"Bye", "seed":"-1","wins":0, "losses":0, "ties":0} #Special case for byes in the bracket.
+                return {"name":"Bye", "seed":-1,"wins":-1, "losses":-1, "ties":-1, "bye":True} #Special case for byes in the bracket.
             print(f"did not find competitor, oops!  Was looking for {playerID}")
 
         for match in matches_nonzero:
             for slot in match["slots"]:
                 if slot["playerID"] != None:
                     player_backfill = playerIDToName(competitors, slot["playerID"])
+
+                    if 'bye' in player_backfill: #exposing it for below filtering.
+                        match['has_bye'] = True
+
                     slot["gscrl_friendly_name"] = player_backfill["name"]
                     slot["gscrl_seed"] = player_backfill["seed"]
                     slot["gscrl_wlt"] = {}
                     slot["gscrl_wlt"]["w"] = player_backfill["wins"]
                     slot["gscrl_wlt"]["l"] = player_backfill["losses"]
                     slot["gscrl_wlt"]["t"] = player_backfill["ties"]
+
+        #Remove any match with a bye built in, so we only care about the real ones as-is.
+        
+        matches_nonzero = [x for x in matches_nonzero if not 'has_bye' in x]
+
 
         return matches_nonzero
