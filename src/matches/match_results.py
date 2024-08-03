6ef315ed48c86639b53ee5f8e75063fa29f30fd5
Kim <MC42@users.noreply.github.com>
2024-08-03 16:52:40 -0400

    Added secret timer work.

--- src/matches/match_results.py
+++ src/matches/match_results.py
@@ -28,13 +28,13 @@ def routeForUpcomingMatches():
         truefinals.getCrossDivisionMatches(arena_settings.tournament_keys)
         .withoutByes()
         .withFilter(lambda x: x["state"] != "done")
-        .withFilter(lambda x: x['state'] == "called")
+        .withFilter(lambda x: x['state'] in ["called", 'ready', 'active'])
         .inOrder(
             lambda x: (
                 x["calledSince"] or float(0),
                 reversor(x["state"] == "unavailable"),
             ),
-            reverse=True,
+            reverse=False, 
         )
         #.toFile("test.json")
         # .withFilter(lambda x: len(x["slots"]) != 0)
